29a5f8d22ed59e0e14629103f4ee8a7dd3378295
Alexand Bubyr <alexandr.bybur@gmail.com>
2021-04-14 18:35:12 +0300

    lesson05 Hard

--- script02.js
+++ script02.js
@@ -1,16 +1,32 @@
 'use strict';
 
-function toString(string) {
-    if (typeof(string) !== 'string') {
-        alert ('Внимание! Не верное знаение функции toString()!');
-        return;
-    } else {
-        string = string.replace(/ /g, '');
-    }
-    if (string.length > 30) {
-        string = string.substring(0, 30)+'...';
-    }
-    return string;
-};
+//Task#1
+let arr = ['132566', '2954', '441223', '45221', '96634', '290056', '77500'];
 
-console.log(toString('Этот текст должен быть достаточно длинным, что бы увидеть как работает функция!'));
+for (let i = 0; i < arr.length; i++) {
+    if (parseInt(arr[i][0]) === 2 || parseInt(arr[i][0]) === 4) {
+        console.log(i, arr[i]);
+    }
+}
+
+//Второй пункт задания
+let baseArray = 100;
+let primeNumbers = [];
+
+for (let j = 2; j <= baseArray; j++) {
+    let factorization = [];
+
+    for (let i = 2; i <= j; i++) {
+        if (j % i === 0) {
+            factorization.push(i);
+        }
+    }
+
+    if (factorization.length === 1) {
+        primeNumbers.push(j);
+    }
+}
+for (let k = 0; k < primeNumbers.length; k++) {
+        console.log(primeNumbers[k] + ' Делители этого числа: 1 и ' + primeNumbers[k]);
+        
+    }
