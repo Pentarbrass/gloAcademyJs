2e66f7b3d8ed89e1c5f132a44f22c36042931c87
Alexand Bubyr <alexandr.bybur@gmail.com>
2021-04-29 09:47:50 +0300

    lesson18p6

--- script.js
+++ script.js
@@ -1,63 +1,42 @@
 'use strict';
+const date = new Date(),
+    days = [
+        'Воскресенье',
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота'
+    ],
+    hours = date.getHours(),
+    day = days[date.getDay()],
+    time = date.toLocaleTimeString('ru'),
+    newDate = new Date(new Date().getFullYear() + 1, 0, 1);
 
-const body = document.querySelector('body');
-function DomElement(selector, height, width, bg, fontSize, position='static') {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-    this.position = position;
-}
+const greeting = document.createElement('p'),
+    dayOfWeek = document.createElement('p'),
+    currentTime = document.createElement('p'),
+    newYearDays = document.createElement('p');
 
-function move(evt)  {
-    const div = document.querySelector('.block');
-    let left = parseInt(div.style.left);
-    let top = parseInt(div.style.top);
-    if (!isNaN(div.style.left)) {
-        left = 0;
-    } 
-    if (!isNaN(div.style.top)) {
-        top = 0;
-    } 
-    if (evt.keyCode === 37) {
-        left += -10;
-        div.style.left = left + 'px';  
-    } else if (evt.keyCode === 38) {
-        top += -10;
-        div.style.top = top + 'px';
-    } else if (evt.keyCode === 39) {
-        left += +10;
-        div.style.left = left + 'px';
-    } else if (evt.keyCode === 40) {
-        top += 10;
-        div.style.top = top + 'px';
-    }
-}
-
-DomElement.prototype.construct = function() {
-    let newElement;
-    if (this.selector[0] === '.') {
-        newElement = document.createElement('div');
-        newElement.classList.add('block');
-    } else if (this.selector[0] === '#') {
-        newElement = document.createElement('p');
-        newElement.id = ('best');
-    }
-    
-    newElement.style.cssText = `
-        height:  ${this.height}px;
-        width:  ${this.width}px;
-        background:  ${this.bg};
-        font-size:  ${this.fontSize}px;
-        position: ${this.position};
-    `;
-    newElement.textContent = 'Hello every one!';
-    body.append(newElement);
-    document.addEventListener('keydown', move);
+const changeEnding = (num) => {
+    const textVariant = [' день', ' дня', ' дней'];
+    const n1 = num % 100,
+        n2 = num % 10;
+    return n1 > 4 && n1 < 21 ? num + textVariant[2] :
+        n2 === 1 ? num + textVariant[0] :
+        n2 > 1 && n2 < 5 ? num + textVariant[1] :
+        num + textVariant[2];
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-    let newDomSquare = new DomElement('.id', 100, 100, 'gold', 15, 'absolute');
-    newDomSquare.construct();
-});
+
+greeting.textContent = hours < 5 || hours > 22 ? 'Доброй ночи' :
+    hours < 10 ? 'Доброе утро' :
+    hours < 17 ? 'Добрый день' :
+    'Добрый вечер';
+dayOfWeek.textContent = 'Сегодня: ' + day;
+currentTime.textContent = 'Текущее время: ' + time;
+newYearDays.textContent = 'До нового года осталось ' + 
+    changeEnding(Math.ceil((newDate.getTime() - date.getTime()) / 1000 / 60 / 60 / 24));
+
+document.body.append(greeting, dayOfWeek, currentTime, newYearDays);
