c4383b384b03f2512af902b73ed11ce5d7a99288
Alexand Bubyr <alexandr.bybur@gmail.com>
2021-04-30 11:55:50 +0300

    lesson19 Task 1&2

--- script/script.js
+++ script/script.js
@@ -42,4 +42,106 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     countTimer('1 May 2021');
+
+    //Animated Scroll
+    const animateScroll = () => {
+
+        let target = event.target.closest('[href^="#"]'),
+            speed = 1;
+
+        if (target) {
+            const pageY = window.pageYOffset,
+                hash = target.href.replace(/[^#]*(.*)/, '$1'),
+                distTopPosition = document.querySelector(hash).getBoundingClientRect().top;
+
+            let start = 0;
+
+            const step = time => {
+                if (!start) start = time;
+
+                const progress = time - start;
+
+                const act = (distTopPosition < 0 ?
+                    Math.max(pageY - progress / speed, pageY + distTopPosition) :
+                    Math.min(pageY + progress / speed, pageY + distTopPosition));
+
+                window.scrollTo(0, act);
+
+                if (act < pageY + distTopPosition) requestAnimationFrame(step);
+            };
+
+            requestAnimationFrame(step);
+
+        }
+    };
+
+    //Menu
+
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+        const handlerMenu = () => {
+            // if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+            //     menu.style.transform = `translate(0)`;
+            // } else {
+            //     menu.style.transform = `translate(-100%)`;
+            // }
+            menu.classList.toggle('active-menu');
+            animateScroll();
+        };
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+    };
+    toggleMenu();
+
+    //Popup
+
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content'),
+            popupData = {
+                count: -445,
+                speed: 12,
+                startPos: -445,
+                endPos: 0
+            };
+
+        const showPopup = () => {
+
+            popupData.startPos > popupData.endPos ?
+                popupData.count -= popupData.speed :
+                popupData.count += popupData.speed;
+            popupContent.style.transform = `translateY(${popupData.count}px)`;
+
+            if (popupData.startPos > popupData.endPos ?
+                popupData.count > popupData.endPos :
+                popupData.count < popupData.endPos) {
+                requestAnimationFrame(showPopup);
+            }
+        };
+
+        popupBtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                popup.style.display = 'block';
+                if (screen.width > 768) {
+                    popupData.count = popupData.startPos;
+                    requestAnimationFrame(showPopup);
+                }
+            });
+        });
+
+        popupClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+        });
+    };
+
+    togglePopUp();
+
+    document.querySelector('main a').addEventListener('click', animateScroll);
+
 });
